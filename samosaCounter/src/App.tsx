ffda8262cf7cf7b7fd3f847ebbdfdd49decd46d8
Matayay Karuna <matayay2@gmail.com>
2023-02-24 17:49:41 -0700

    added click feature to samosa

--- samosaCounter/src/App.tsx
+++ samosaCounter/src/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import samosa from './assets/samosimg.png';
 
 const App = () => {
+    const [count, setCount] = useState(0);
+    const [multiplier, setMultiplier] = useState(1);
+
+    const updateCount = () => {
+        setCount(count + multiplier);
+    };
+
     return (
         <div className='App'>
             <div className='header'>
                 <h1>Samosa Selector</h1>
-                <h2>Count:</h2>
-                <img className='samosa' src={samosa} />
+                <h2>Count: {count}</h2>
+                <img className='samosa' src={samosa} onClick={updateCount} />
             </div>
         </div>
     );
