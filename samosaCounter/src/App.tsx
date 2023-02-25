621203b5677957e6e3bad3588391bcaed1d09834
Matayay Karuna <matayay2@gmail.com>
2023-02-24 17:39:29 -0700

    added samosa image

--- samosaCounter/src/App.tsx
+++ samosaCounter/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import samosa from './assets/samosimg.png';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const App = () => {
+    return (
+        <div className='App'>
+            <div className='header'>
+                <h1>Samosa Selector</h1>
+                <h2>Count:</h2>
+                <img className='samosa' src={samosa} />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
